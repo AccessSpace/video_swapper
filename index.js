ed51e6f5ef6f3dcfc0806e53c97e76af9740bcee
Martyn Eggleton <martyn.eggleton@gmail.com>
2018-06-28 16:07:30 +0000

    Adds Logical Debouncing, with 2 seconds

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-const omx = require('omx-layers');
+var pigpio = require('pigpio')
+var Gpio = pigpio.Gpio;
+try{
+	pigpio.initialize();
+} catch {
+	pigpio.terminate();
+	process.exit();
+}
+process.on('SIGINT', function () {
+	layers[0].stop();
+	layers[1].stop();
+	pigpio.terminate(); // pigpio C library terminated here
 
-var Gpio = require('pigpio').Gpio,
-  button = new Gpio(4, {
+});
+
+var LogicalDebounceTimeMS = 2000;
+
+var  button = new Gpio(4, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_DOWN,
-    edge: Gpio.RISING_EDGE
+    edge: Gpio.RISING_EDGE,
+    alert:true
   }),
   button2 = new Gpio(22, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_DOWN,
-    edge: Gpio.RISING_EDGE
+    edge: Gpio.RISING_EDGE,
+    alert:true
   });
   
-button.glitchFilter(100000);
-button2.glitchFilter(100000);
+button.glitchFilter(10000);
+button2.glitchFilter(10000);
+
+
+var lastStateChange = 0;
+
 
 if(button.digitalRead() ==1 && button2.digitalRead() ==1 ){
 	process.exit();
-
 }
+
+const omx = require('omx-layers');
+
 
 let layers = [];
 const numLayers = 2;
@@ -36,11 +58,31 @@ for (var i=0; i<numLayers; i++) {
     );
 }
 
+
+button.on('alert', function (value, tick) {
+	console.log("button", value);
+		toggleVideo(); 
+});
+
+button2.on('alert', function (value,tick) {
+	console.log("button2", value);
+		toggleVideo(); 
+});
+
+
+
+
 layers[0].open('/home/pi/video_swapper/Alternative Architecture 02_2 version.mp4');
 
 layers[1].open('/home/pi/video_swapper/white_noise.mp4.mp4');
 
 function toggleVideo(){
+	var newTime = Date.now();
+	console.log(newTime , lastStateChange,newTime - lastStateChange);
+	if ((newTime - lastStateChange) < LogicalDebounceTimeMS){
+		return;
+	}
+	lastStateChange = newTime;
 	layers[1].getPlayStatus().then( status => {
 		console.log("status", status);
 		if(status == "playing"){
@@ -56,19 +98,3 @@ function toggleVideo(){
 		}
 	});
 };
-
-button.on('interrupt', function (value) {
-	console.log("button", value);
-		toggleVideo(); 
-});
-
-button2.on('interrupt', function (value) {
-	console.log("button", value);
-		toggleVideo(); 
-});
-
-process.on('SIGINT', function () {
-	layers[0].stop();
-	layers[1].stop();
-	//button.unexport();
-});
